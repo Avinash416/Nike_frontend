ffde65cc0e0558c441a9872f811e68ebe1b8dfaf
Avinash416 <90385785+Avinash416@users.noreply.github.com>
2024-05-31 12:20:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
     images: {
         domains: ["res.cloudinary.com"],
     },
+     experimental: {
+        pageDataCollectionTimeout: 300000, // Set this value as needed (in milliseconds)
+    },
 };
 
 module.exports = nextConfig;
